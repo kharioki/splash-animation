55e9ad08ad707e5886fce61f9ebe34ed3ffb5c7b
kharioki <tony.kharioki@gmail.com>
2023-01-09 08:32:08 +0300

    edit splash screen and use edges for home component

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,5 @@
-import { Animated, Dimensions, Image, ScrollView, Text, View } from 'react-native'
+import { Dimensions, Image, ScrollView, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 // Gallery ..
 import Locs from '../assets/locs.png'
@@ -6,13 +7,16 @@ import Date from '../assets/date.jpg'
 import Selfie from '../assets/selfie.jpg'
 
 const Home = () => {
+
+  const edges = useSafeAreaInsets();
+
   return (
     <View>
       <ScrollView>
         <View style={{
           paddingLeft: 15,
           paddingRight: 15,
-          paddingTop: 100,
+          paddingTop: (edges.top + 65),
           paddingBottom: 25,
         }}>
           {
